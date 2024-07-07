0e582a8cb1600d0dd727764e5d0e106ac3a64d45
AkanshaRaw <166435068+AkanshaRaw@users.noreply.github.com>
2024-07-07 06:36:57 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "98abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnum = Number (score)
+
+console.log(typeof valueinnum);
+console.log(valueinnum);
+
+
+// "98" = 98
+// "98abc" = NaN 
+// true = 1; false =0;
+// string = NaN 
+
+
+let isnum = "akansha"
+
+let booleanisnum = Boolean(isnum)
+console.log(booleanisnum);
+
+
+// true = 1; false =0;
+// "akansha"=1;
+//"" = 0
+
+let num = 98
+let stringnum = String( num)
+console.log(stringnum);
+console.log(typeof stringnum);
