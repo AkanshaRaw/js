4502c19c26ced40defbcab54642105d7117b0d44
AkanshaRaw <166435068+AkanshaRaw@users.noreply.github.com>
2024-07-07 06:23:27 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat JS as newer version
+
+let age =18
+let name = "akansha"
+let isname= false
+
+//number = 2 to power 53
+//string = ""
+// boolean = true / false
+// null = stand alone value 
+//undefined 
+// symbol = unique
+
+
+
+// object 
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined 
