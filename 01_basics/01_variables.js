52c61c0b6271e9cee091a4579498c883f8baa10f
AkanshaRaw <166435068+AkanshaRaw@users.noreply.github.com>
2024-07-07 06:11:15 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14379
+let accountenamil ="akansha@gmail.com"
+var accountpass= "183293029"
+accountcity = "mumbai"
+
+//accountId = 3 // not allowed 
+
+/* not to use var 
+coz of issue of scope 
+*/
+
+
+accountenamil="ansjwi@gmail.com"
+accountcity="benglauru"
+
+
+console.log(accountId);
+ console.table([accountId,accountenamil,accountpass,accountcity])
