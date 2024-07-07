6888b8141fc5bb5848f67e423542529b7fc1cd06
AkanshaRaw <166435068+AkanshaRaw@users.noreply.github.com>
2024-07-07 09:27:20 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,25 @@
+ // 1.primitive = inko copy krke diya jata hai jo bhi changes hote hai toh voh copy wale data se hote hai
+ // 7 types : String , Number, Boolean , Null , Undefined , Symbol(kise bhi value ko unique banane ke liye),  BigInt 
+
+ const id = Symbol('123')
+ const anotherid = Symbol('123')
+
+ console.log(id === anotherid);
+
+
+ //2.refrence (non primitive) (memory ka refrence directly allocate kiya jata hai)
+ // arrays , objects , functions 
+
+
+ const heros=["shaktiman" , "doga"];
+ let myObj={
+    name:"akansha",
+    age: 18,
+
+ }
+
+ const myFunction = function(){
+    console.log("hello world");
+ }
+
+ console.log(typeof );
