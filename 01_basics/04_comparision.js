fe7ec4cd336e7ffe9b81a97a89ff7696530e2fec
AkanshaRaw <166435068+AkanshaRaw@users.noreply.github.com>
2024-07-07 06:57:48 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+// console.log(2>1);
+//console.log(2>=1);
+//console.log(2==1);
+//console.log(2<1);
+
+console.log("2"> 1);
+//avois this type of code 
+console.log(null > 0); 
+console.log(null==0);
+console.log(null >=0); // null covert into number hence 0 ban gya then compare krne pr true aya 
+
+// === checks the value strictly by also checking the datatype 
+
+console.log( "2" === 2);
