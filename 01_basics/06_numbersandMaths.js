293e31e7f348438f791c218b96d93f2f8ec49ddb
AkanshaRaw <166435068+AkanshaRaw@users.noreply.github.com>
2024-07-08 05:45:22 +0000

    math and num

---
+++ 01_basics/06_numbersandMaths.js
@@ -0,0 +1,33 @@
+
+/*const score = 100 
+const balance = new Number (100)
+console.log(balance);
+
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const othernumber = 123.45
+
+console.log(othernumber.toPrecision(3));
+
+const hundred = 10000000
+console.log(hundred.toLocaleString('en-IN'));
+*/
+
+///////////////////////////////MATHS/////////////////////////////////////////
+//console.log(Math);
+//console.log(Math.abs(-6));
+//console.log(Math.round(4.7)); // .floor() and .ceil()
+//console.log(Math.min(4,8,9,7)); 
+//console.log(Math.max(4,5,7)); 
+
+
+// random ki value hmesha 1 n 0 aati hai 
+//console.log(Math.floor(Math.random()*10)+1); // jab 1 n 0 nai chahiye aur kuch aur chahiye // to avoid 0.4 ka result as a 0 hence hmne 1 add kiya ,,,,1 se 9 
+
+
+const min =10
+const max= 20 
+
+console.log(Math.floor(Math.random() * (max - min +1 ) ) + min);  // so that 0 value na aaye // max - min so that range aa jaye konsi range mai mujhe ans chahiye 
